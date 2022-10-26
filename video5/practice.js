e0ec475074b5cfdd228aad6ce6da5f6a4270a506
Arif-Hossain049 <arifhossainwasim049@gmail.com>
2022-10-27 00:04:11 +0600

    adding video 5

---
+++ video5/practice.js
@@ -0,0 +1,28 @@
+// chapter 1 q1
+const a = "Mr Wasim"
+const b = 2
+console.log(a+b)
+
+// chapter 1 q2
+
+console.log(typeof (a+b))
+
+
+// chapter 1 q3
+const a1 = {
+    name: "Wasim",
+    section: 1,
+    isPrincipal:false
+}
+a1  ["friend"] = ["Jahangir"]
+
+// chapter 1 q4
+
+const dict = {
+    jahangir: "Jahangir is a sentikhor pola",
+    moMin:"amder borisailla dadu",
+    rudro:"somon ar jawra pola ammo koise",
+    Yeamin:"Bhodro and itor",
+    RafSan:"bolod bolod bhab but khub chalak"
+}
+console.log(dict)
